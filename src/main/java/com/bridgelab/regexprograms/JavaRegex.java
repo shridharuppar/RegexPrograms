bf381d77309d7db4846aa70139a5bd1183f56e45
shridharuppar <upparshridhar@gmail.com>
2023-04-16 23:00:15 +0530

    add :check the valid Email

--- src/main/java/com/bridgelab/regexprograms/JavaRegex.java
+++ src/main/java/com/bridgelab/regexprograms/JavaRegex.java
@@ -9,26 +9,35 @@ import java.util.regex.Pattern;
 public class JavaRegex {
 	
 
+	public static void main(String[] args) {
+		String regex = "^[A-Z][a-zA-Z]{2,}$";
+		String regex1 = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The First Name : ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter The Last Name : ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter Email Address: ");
+		String data = sc.nextLine();
+
+		Pattern pattern = Pattern.compile(regex1);
+
+		Matcher matcher = pattern.matcher(firstName + lastName);
+		Matcher matcher1 = pattern.matcher(data);
+		boolean result = matcher1.matches();
+
+		if (result) {
+			System.out.println("Valid first name and last name");
+			System.out.println("Valid Email");
+		} else {
+			System.out.println("Pattern not matched");
+			System.out.println("Invalid Email");
+		}
+
+	}
+
+}
 	
-		public static void main(String[] args) {
-		    String regex = "^[A-Z][a-zA-Z]{2,}$";
-
-		    Scanner sc = new Scanner(System.in);
-		    System.out.println("Enter The First Name : ");
-			String data = sc.nextLine();
-
-
-			Pattern pattern = Pattern.compile(regex);
-
-			Matcher matcher = pattern.matcher(data);
-			boolean result = matcher.matches();
-
-		    if (result) {
-		        System.out.println("Valid first name");
-		    } else {
-		        System.out.println("Pattern not matched");
-		    }
-
-	}
-	}
+	
 
