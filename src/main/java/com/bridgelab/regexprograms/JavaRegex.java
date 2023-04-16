9f22b94f069ce132ed8688d46ba56d156950225f
shridharuppar <upparshridhar@gmail.com>
2023-04-16 23:06:40 +0530

    add :check the country code with mobile number

--- src/main/java/com/bridgelab/regexprograms/JavaRegex.java
+++ src/main/java/com/bridgelab/regexprograms/JavaRegex.java
@@ -10,33 +10,29 @@ public class JavaRegex {
 	
 
 	public static void main(String[] args) {
-		String regex = "^[A-Z][a-zA-Z]{2,}$";
-		String regex1 = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+		String regex1 = "^[1-9][0-9][1-9][0-9]{9}$";
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter The First Name : ");
-		String firstName = sc.nextLine();
-		System.out.println("Enter The Last Name : ");
-		String lastName = sc.nextLine();
 		System.out.println("Enter Email Address: ");
 		String data = sc.nextLine();
+		System.out.println("Enter Mobile Number: ");
+		String mobileNO = sc.nextLine();
 
 		Pattern pattern = Pattern.compile(regex1);
-
-		Matcher matcher = pattern.matcher(firstName + lastName);
-		Matcher matcher1 = pattern.matcher(data);
+		Matcher matcher = pattern.matcher(data);
+		Matcher matcher1 = pattern.matcher(mobileNO);
 		boolean result = matcher1.matches();
 
 		if (result) {
-			System.out.println("Valid first name and last name");
 			System.out.println("Valid Email");
+			System.out.println("Valid Mobile Number");
 		} else {
-			System.out.println("Pattern not matched");
 			System.out.println("Invalid Email");
+			System.out.println("Invalid Mobile Number");
 		}
 
 	}
-
 }
 	
 	
